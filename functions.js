5aaab8ad7a4c807aa7dcada793fe10d637ca947c
Cesar Solano <cesar.solano@ghsiss.com>
2023-08-13 10:31:58 -0700

    algodon

---
+++ functions.js
@@ -0,0 +1,15 @@
+// Get a reference to the body element
+const body = document.body;
+
+function changeBackgroundImage() {
+  const currentFileName = location.pathname.split('/').slice(-1)[0];
+
+  if (currentFileName === 'menu.html') {
+    body.style.backgroundImage = "url('./assets/menu.jpeg')";
+  } else {
+    body.style.backgroundImage = "url('./assets/gottlogo.png')";
+  }
+}
+
+// Call the function to change the background image
+changeBackgroundImage();
